e6b951d11836601bfc17a99f230f1e8e5cff546b
steveninhle <lephuocninh7118@gmail.com>
2025-04-08 22:41:36 +0700

    feat: install google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import AppProvider from "@/store/app.store";
 import { ReactNode } from "react";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Rafl",
@@ -27,9 +28,29 @@ export default async function RootLayout({
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MBR9TGCF');
+          `,
+          }}
+        />
       </Head>
       <AppProvider>
         <body suppressHydrationWarning={true}>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-MBR9TGCF`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Toaster />
           <DefaultLayout>{children}</DefaultLayout>
         </body>
