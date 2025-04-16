bd29d9c7ebf73677ea96866e5e545fd9e510394a
steveninhle <lephuocninh7118@gmail.com>
2025-04-16 10:19:21 +0700

    fix: install GTM with next third parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import AppProvider from "@/store/app.store";
 import { ReactNode } from "react";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Rafl",
@@ -28,24 +28,18 @@ export default async function RootLayout({
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MBR9TGCF');
-          `,
-          }}
-        />
       </Head>
       <AppProvider>
+        <GoogleTagManager gtmId="GTM-MBR9TGCF" />
         <body suppressHydrationWarning={true}>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBR9TGCF"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MBR9TGCF"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Toaster />
           <DefaultLayout>{children}</DefaultLayout>
         </body>
